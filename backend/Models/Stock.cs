d8de74329c06764e190d44f30cedc7bb8b6035d2
Rushi_0508 <rushigandhi14@gmail.com>
2024-05-05 17:56:40 +0530

    Added Models

---
+++ backend/Models/Stock.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; } = String.Empty;
+        public string CompanyName { get; set; } = String.Empty;
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Purchase { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal LastDiv { get; set; }
+        public string Industry { get; set; } = String.Empty;
+        public long MarketCap { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
