0cd3b6b68322e7309c0e221f0228680fdf075925
Rushi_0508 <rushigandhi14@gmail.com>
2024-05-08 17:40:49 +0530

    Completed Stock delete

--- backend/Controllers/StockController.cs
+++ backend/Controllers/StockController.cs
@@ -67,5 +67,21 @@ namespace backend.Controllers
 
             return Ok(stockModel.ToStockDto());
         }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stocks.Remove(stockModel);
+            _context.SaveChanges();
+            return NoContent();
+        }
     }
 }
